9e18ebd15f7f5c3ed911d5557dcea6dc48e4ade9
Andrew McDermott <aim@frobware.com>
2025-08-12 11:50:26 +0100

    Add internal/doc.go for CEL filtering tests
    This operator-only change should trigger only the operator pipeline
    due to CEL expression filtering on internal/*** path changes.

---
+++ internal/doc.go
@@ -0,0 +1,5 @@
+// Package internal contains the internal implementation of the TodoApp operator.
+// This file is used for testing CEL expression filtering for operator-only changes.
+//
+// Test changes:
+// - Change added on Mon Aug 12 10:48:15 UTC 2025
