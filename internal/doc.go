2f7345767a9a2d010bb9b86c4db23ac256a274d9
Andrew McDermott <aim@frobware.com>
2025-08-13 09:45:02 +0100

    Test nudging behaviour with plain text file
    Add bundle-hack/foo.txt containing an image reference to test whether
    the nudging mechanism updates plain text files alongside shell scripts.
    Update the build-nudge-files annotation to include the new file and
    add a test change to internal/doc.go to trigger the pipeline.

--- internal/doc.go
+++ internal/doc.go
@@ -5,3 +5,4 @@
 // - Change added on Mon Aug 12 10:48:15 UTC 2025
 // - Change added on Tue 12 Aug 14:54:48 BST 2025
 // - Change added on Tue 12 Aug 15:07:30 BST 2025 (testing nudging with annotations in place)
+// - Change added on Tue 13 Aug 2025 (testing plain text file nudging pattern)
